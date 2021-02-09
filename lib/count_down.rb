f100d3e4fa9ec26b65b7bb8bd93317c52a1be2d9
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-09 21:02:29 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
 
-count = 0 
-while count < 3 do 
-  puts "I am the #{count}, I love to count!"
-  count +=1 
+count = 10 
+while count > 0 do 
+  puts "#{count}"
+  count -=1 
 end 
